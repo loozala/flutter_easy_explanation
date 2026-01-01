428a8d8578d89cbed49fcea9d8d237bbb775b249
ursche <urjalapariyar@gmail.com>
2026-01-01 17:08:00 +0545

    landing page

---
+++ .dart_tool/flutter_build/f2ee78612938830f50d8df87a4c1841e/dart_build.d
@@ -0,0 +1,1 @@
+ C:\\Users\\HP\\flutter_easy_explanation\\.dart_tool\\flutter_build\\f2ee78612938830f50d8df87a4c1841e\\dart_build_result.json: 
---
+++ lib/landing_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            // colors: [Color(0xff2596b3), Color(0xff114558)],
+            colors: [Color(0xff2492ba), Color(0xff15566d)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Container(
+                alignment: Alignment.center,
+                width: 210,
+                height: 210,
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(300),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xdd2596be),
+                      spreadRadius: 50,
+                      // blurRadius: 30,
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    'E',
+                    style: TextStyle(
+                      color: Color(0xff2596b3),
+                      fontSize: 150,
+                      fontWeight: FontWeight.w900,
+                      height: 1.0, // Removes extra vertical space
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
